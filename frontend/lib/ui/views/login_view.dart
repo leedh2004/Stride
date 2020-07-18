9885831c23af3a1ceeb02fb27f9559bd990b7d42
이도현 <leedh2008@naver.com>
2020-07-18 12:45:28 +0900

    로그인뷰 Gradient 추가

--- frontend/lib/ui/views/login_view.dart
+++ frontend/lib/ui/views/login_view.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/core/constants/app_constants.dart';
+import 'package:frontend/ui/shared/app_colors.dart';
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Login View'),
-          FlatButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, RoutePaths.Home);
-              },
-              color: Colors.blue,
-              child: Text('Go to Home'))
-        ],
-      )),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [gradientStart, backgroundColor])),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Login View'),
+            FlatButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, RoutePaths.Home);
+                },
+                color: Colors.blue,
+                child: Text('Go to Home'))
+          ],
+        )),
+      ),
     );
   }
 }
