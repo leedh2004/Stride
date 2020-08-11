12bb07461ef87c09c1043d0643e2761a5114789f
shinyooklee <tlsdbr97@gmail.com>
2020-08-11 23:47:34 +0900

    Add: user concept update query #34

--- backend/db/queries/user.py
+++ backend/db/queries/user.py
@@ -27,16 +27,6 @@ def insert_user_birth(year):
     cursor.close()
 
 
-def update_user_size(size):
-    cursor = service_conn.cursor()
-    try:
-        service_conn.commit()
-    except:
-        service_conn.rollback()
-        pass
-    cursor.close()
-
-
 def update_login_timestamp(user_id):
     cursor = service_conn.cursor()
     query = """update users set last_login_at = CURRENT_TIMESTAMP where user_id = %s"""
@@ -66,6 +56,21 @@ def update_user_size(size):
     query = """UPDATE users SET length = %s , waist = %s , hip = %s, thigh = %s, rise = %s, hem = %s, shoulder = %s, bust = %s, arm_length = %s where id = %s"""
     try:
         load = (size['length'], size['waist'], size['hip'], size['thigh'], size['rise'], size['hem'], size['shoulder'], size['bust'], size['arm_length'], g.user_id)
+        cursor.execute(query, load)
+        service_conn.commit()
+    except:
+        service_conn.rollback()
+        raise
+        pass
+    finally:
+        cursor.close()
+
+
+def update_user_concept(user_id, concept):
+    cursor = service_conn.cursor()
+    query = """UPDATE users SET shop_concept = %s WHERE user_id = %s"""
+    try:
+        load = (concept, user_id)
         cursor.execute(query, load)
         service_conn.commit()
     except:
