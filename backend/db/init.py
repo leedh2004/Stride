3bb6b8fc204fb442683e6f255424db8775a87a28
leeshinyook <tlsdbr97@gmail.com>
2020-08-14 16:29:37 +0900

    Fix: DB Connection timeout error #44

--- backend/db/init.py
+++ backend/db/init.py
@@ -1,14 +1,23 @@
-import pymongo
 import psycopg2
 import sys
 sys.path.append('../')
 sys.path.append('../../')
 sys.path.append('../../../')
 from config.oauthconfig import *
+from contextlib import contextmanager
 
-service_conn = psycopg2.connect(
-    database=PSQL_DB,
-    user=PSQL_User,
-    password=PSQL_PW,
-    host=PSQL_Host,
-    port=PSQL_Port)
+
+@contextmanager
+def db_connect():
+    service_conn = psycopg2.connect(
+        database=PSQL_DB,
+        user=PSQL_User,
+        password=PSQL_PW,
+        host=PSQL_Host,
+        port=PSQL_Port)
+    cursor = service_conn.cursor()
+    try:
+        yield (service_conn, cursor)
+    finally:
+        cursor.close()
+        service_conn.close()
--- backend/db/queries/home.py
+++ backend/db/queries/home.py
@@ -5,52 +5,49 @@ from bson import json_util
 
 
 def get_home_clothes():
-    cursor = service_conn.cursor()
-    query = """SELECT * FROM Products ORDER BY random() LIMIT 20"""
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        product = []
-        for item in result:
-            load = {}
-            load['product_id'] = item[0]
-            load['product_url'] = item[3]
-            load['product_name'] = item[4]
-            load['price'] = item[5]
-            load['thumbnail_url'] = item[6]
-            load['image_url'] = item[7]
-            load['type'] = item[8]
-            product.append(load)
-        cursor.close()
-        return json.dumps(product, default=json_util.default, ensure_ascii=False)
-    except:
-        pass
-    return 'None'
+    with db_connect() as (service_conn, cursor):
+        query = """SELECT * FROM Products ORDER BY random() LIMIT 20"""
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            product = []
+            for item in result:
+                load = {}
+                load['product_id'] = item[0]
+                load['product_url'] = item[3]
+                load['product_name'] = item[4]
+                load['price'] = item[5]
+                load['thumbnail_url'] = item[6]
+                load['image_url'] = item[7]
+                load['type'] = item[8]
+                product.append(load)
+            return json.dumps(product, default=json_util.default, ensure_ascii=False)
+        except:
+            pass
+        return 'None'
 
 
 def get_clothes_category(type):
-    cursor = service_conn.cursor()
-    query = """SELECT * FROM Products WHERE type = %s ORDER BY random() LIMIT 20"""
-    try:
-        cursor.execute(query, (type,))
-        result = cursor.fetchall()
-        product = []
-        for item in result:
-            load = {}
-            load['product_id'] = item[0]
-            load['product_url'] = item[3]
-            load['product_name'] = item[4]
-            load['price'] = item[5]
-            load['thumbnail_url'] = item[6]
-            load['image_url'] = item[7]
-            load['type'] = item[8]
-            product.append(load)
-        cursor.close()
-        return json.dumps(product, default=json_util.default, ensure_ascii=False)
-    except:
-        pass
-    return 'None'
-
+    with db_connect() as (service_conn, cursor):
+        query = """SELECT * FROM Products WHERE type = %s ORDER BY random() LIMIT 20"""
+        try:
+            cursor.execute(query, (type,))
+            result = cursor.fetchall()
+            product = []
+            for item in result:
+                load = {}
+                load['product_id'] = item[0]
+                load['product_url'] = item[3]
+                load['product_name'] = item[4]
+                load['price'] = item[5]
+                load['thumbnail_url'] = item[6]
+                load['image_url'] = item[7]
+                load['type'] = item[8]
+                product.append(load)
+            return json.dumps(product, default=json_util.default, ensure_ascii=False)
+        except:
+            pass
+        return 'None'
 
 
 
