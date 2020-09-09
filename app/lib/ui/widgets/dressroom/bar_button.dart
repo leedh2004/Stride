4921d755ee802c5b45b0ebe85033502dbda15b75
이도현 <leedh2008@naver.com>
2020-09-09 20:03:56 +0900

    튜토리얼 UI완성, 이후에 WidgetBinding 수정

--- app/lib/ui/widgets/dressroom/bar_button.dart
+++ app/lib/ui/widgets/dressroom/bar_button.dart
@@ -4,6 +4,7 @@ import 'package:app/core/viewmodels/views/dress_room.dart';
 import 'package:app/main.dart';
 import 'package:app/ui/shared/app_colors.dart';
 import 'package:app/ui/shared/text_styles.dart';
+import 'package:app/ui/shared/ui_helper.dart';
 import 'package:app/ui/widgets/dressroom/folder_dialog.dart';
 import 'package:app/ui/widgets/dressroom/select_dialog.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
@@ -12,21 +13,45 @@ import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
-class DressRoomButtonBar extends StatelessWidget {
+class DressRoomButtonBar extends StatefulWidget {
   final DressRoomModel model;
   DressRoomButtonBar(this.model);
+  @override
+  _DressRoomButtonBarState createState() => _DressRoomButtonBarState();
+}
+
+class _DressRoomButtonBarState extends State<DressRoomButtonBar> {
   TextEditingController _textController = TextEditingController();
+  GlobalKey folderButton = GlobalKey();
+  GlobalKey makeButton = GlobalKey();
+  GlobalKey trashButton = GlobalKey();
+
+  TutorialCoachMark tutorialCoachMark;
+  List<TargetFocus> targets = List();
+
+  @override
+  void initState() {
+    initTargets();
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
+    super.initState();
+  }
+
+  @override
+  void dispopse() {
+    super.dispose();
+  }
 
   void _handleSubmitted(String text) {
     // print(_textController.text);
-    model.createFolder(_textController.text);
+    widget.model.createFolder(_textController.text);
     _textController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (model.selectedIdx.isNotEmpty) {
+    if (widget.model.selectedIdx.isNotEmpty) {
       return Container(
         padding: EdgeInsets.all(8),
         child: Stack(
@@ -53,13 +78,13 @@ class DressRoomButtonBar extends StatelessWidget {
                               backgroundColor: Colors.transparent,
                               context: context,
                               builder: (context, scrollcontext) {
-                                return FolderDialog(model);
+                                return FolderDialog(widget.model);
                               });
                         },
                       ),
                     ))),
-            model.top_cnt > 0 && model.bottom_cnt > 0
-                ? MakeButton(model, context)
+            widget.model.top_cnt > 0 && widget.model.bottom_cnt > 0
+                ? MakeButton(widget.model, context)
                 : DisableMakeButton(),
             Align(
               alignment: Alignment.centerRight,
@@ -122,6 +147,7 @@ class DressRoomButtonBar extends StatelessWidget {
                 child: ButtonTheme(
                   minWidth: 46,
                   height: 46,
+                  key: folderButton,
                   child: FlatButton(
                     padding: EdgeInsets.all(0),
                     color: Color.fromRGBO(245, 242, 253, 1),
@@ -142,7 +168,7 @@ class DressRoomButtonBar extends StatelessWidget {
                           backgroundColor: Colors.transparent,
                           context: context,
                           builder: (context, scrollController) {
-                            return FolderDialog(model);
+                            return FolderDialog(widget.model);
                           });
                     },
                   ),
@@ -155,6 +181,7 @@ class DressRoomButtonBar extends StatelessWidget {
                 child: Container(
                     margin: EdgeInsets.only(right: 30),
                     child: ButtonTheme(
+                        key: trashButton,
                         minWidth: 46,
                         height: 46,
                         child: FlatButton(
@@ -171,44 +198,181 @@ class DressRoomButtonBar extends StatelessWidget {
       );
     }
   }
-}
 
-Widget MakeButton(DressRoomModel model, BuildContext context) {
-  return (Align(
-    alignment: Alignment.center,
-    child: FlatButton(
-        padding: EdgeInsets.fromLTRB(70, 12, 70, 12),
+  void initTargets() {
+    targets.add(
+      TargetFocus(
+        identify: "Target 0",
+        keyTarget: folderButton,
         color: backgroundColor,
-        onPressed: () async {
-          List<Product> top = model.findSelectedTop();
-          List<Product> bottom = model.findSelectedBotoom();
-          Stride.analytics.logEvent(name: "DRESS_ROOM_MAKE_BUTTON_CLICKED");
-          showMaterialModalBottomSheet(
-              backgroundColor: Colors.transparent,
-              expand: false,
-              context: context,
-              builder: (context, scrollController) =>
-                  DressRoomSelectDialog(top, bottom));
-        },
-        child: Text(
-          'MAKE',
-          style: whiteStyle,
-        )
-        // child: Text('Make', style: whiteStyle),
-        ),
-  ));
+        contents: [
+          ContentTarget(
+              align: AlignContent.top,
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "폴더 버튼",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        "폴더로 드레스룸을 편하게 관리할 수 있습니다.",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+    targets.add(
+      TargetFocus(
+        identify: "Target 1",
+        keyTarget: makeButton,
+        color: backgroundColor,
+        contents: [
+          ContentTarget(
+              align: AlignContent.top,
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "MAKE BUTTON",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        "상/하의를 클릭하면 활성화됩니다.\n 상/하의를 조합하여 나만의 룩북을 만들 수 있습니다.",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    UIHelper.verticalSpaceLarge,
+                    UIHelper.verticalSpaceMedium,
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+
+    targets.add(
+      TargetFocus(
+        identify: "Target 2",
+        keyTarget: trashButton,
+        color: backgroundColor,
+        contents: [
+          ContentTarget(
+              align: AlignContent.top,
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      "휴지통",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        "선택된 옷들을 모두 삭제합니다.",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+
+  void showTutorial() {
+    tutorialCoachMark = TutorialCoachMark(context,
+        targets: targets,
+        colorShadow: Colors.red,
+        textSkip: "SKIP",
+        hideSkip: true,
+        paddingFocus: 10,
+        opacityShadow: 0.8, onFinish: () {
+      print("finish");
+    }, onClickTarget: (target) {
+      print(target);
+    }, onClickSkip: () {
+      print("skip");
+    })
+      ..show();
+  }
+
+  void _afterLayout(_) {
+    Future.delayed(Duration(milliseconds: 1000), () {
+      print('wtf');
+      showTutorial();
+    });
+  }
+
+  Widget DisableMakeButton() {
+    return (Align(
+      alignment: Alignment.center,
+      child: FlatButton(
+          key: makeButton,
+          padding: EdgeInsets.fromLTRB(70, 12, 70, 12),
+          color: Colors.black26,
+          onPressed: () {},
+          child: Text(
+            'MAKE',
+            style: whiteStyle,
+          )),
+    ));
+  }
+
+  Widget MakeButton(DressRoomModel model, BuildContext context) {
+    return (Align(
+      alignment: Alignment.center,
+      child: FlatButton(
+          padding: EdgeInsets.fromLTRB(70, 12, 70, 12),
+          color: backgroundColor,
+          onPressed: () async {
+            List<Product> top = model.findSelectedTop();
+            List<Product> bottom = model.findSelectedBotoom();
+            Stride.analytics.logEvent(name: "DRESS_ROOM_MAKE_BUTTON_CLICKED");
+            showMaterialModalBottomSheet(
+                backgroundColor: Colors.transparent,
+                expand: false,
+                context: context,
+                builder: (context, scrollController) =>
+                    DressRoomSelectDialog(top, bottom));
+          },
+          child: Text(
+            'MAKE',
+            style: whiteStyle,
+          )
+          // child: Text('Make', style: whiteStyle),
+          ),
+    ));
+  }
 }
 
-Widget DisableMakeButton() {
-  return (Align(
-    alignment: Alignment.center,
-    child: FlatButton(
-        padding: EdgeInsets.fromLTRB(70, 12, 70, 12),
-        color: Colors.black26,
-        onPressed: () {},
-        child: Text(
-          'MAKE',
-          style: whiteStyle,
-        )),
-  ));
-}
+// class DressRoomButtonBar extends StatelessWidget {
+//   final DressRoomModel model;
+//   DressRoomButtonBar(this.model);
+//   TextEditingController _textController = TextEditingController();
+
+// }
