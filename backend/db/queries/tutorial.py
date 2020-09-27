12dea7cea8ca72ef8ec34b457f20a65d1673caac
shinyooklee <tlsdbr97@gmail.com>
2020-09-27 23:50:05 +0900

    Add: 사용자 컨셉 추출 및 저장 #104

--- backend/db/queries/tutorial.py
+++ backend/db/queries/tutorial.py
@@ -1,5 +1,6 @@
 from backend.db.init import *
 from backend.db.model.tutorial import *
+from backend.module.concept import Concept
 from flask import g
 import json
 from bson import json_util
@@ -17,6 +18,30 @@ def get_random_list():
                 load.fetch_data(item)
                 product.append(load.__dict__)
             return json.dumps(product, default=json_util.default, ensure_ascii=False)
+        except Exception as Ex:
+            print(Ex)
+            raise
+
+
+def extract_and_update_concept(product_id):
+    with db_connect() as (service_conn, cursor):
+        extract_query = """
+        SELECT shop_concept, count(*)
+        FROM (
+          SELECT unnest(shop_concept) AS shop_concept
+          FROM products p JOIN shop s ON p.shop_id = s.shop_id WHERE product_id IN %s) t
+        GROUP BY shop_concept
+        """
+        update_query = """
+        UPDATE users SET shop_concept = %s WHERE user_id = %s
+        """
+        try:
+            cursor.execute(extract_query, (tuple(product_id),))
+            result = cursor.fetchall()
+            cal_result = Concept.calc_(result)
+            cursor.execute(update_query, (cal_result, g.user_id))
+            service_conn.commit()
+            return True
         except Exception as Ex:
             print(Ex)
             raise
